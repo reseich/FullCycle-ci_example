d043065d06dfee7bdde5aa829f18fc9141087361
Rafael Eich da Silva <reseich@gmail.com>
2021-03-04 23:24:55 -0400

    fix: fail ci

--- math_test.go
+++ math_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSoma(t *testing.T) {
-	total := Sum(15, 12)
+	total := Sum(15, 15)
 
 	if total != 30 {
 		t.Errorf("Result wrong. Result:%d | Expected %d", total, 30)
